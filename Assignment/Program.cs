d14d6f9a83233e36ba6b44ab0988e7c08d5ed251
visualstudio <visualstudio@122-PC22>
2019-03-06 10:10:38 -0500

    Lab01

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -10,6 +10,26 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
+            DelegateExercises o = new DelegateExercises();
+            o.Method2();
+        }
+    }
+    public class DelegateExercises
+    {
+        //Name : Gurkirat Singh (C0731779)
+        //Name : Robindeep Singh (C0723381)
+        public delegate void MyDelegate();
+
+        void Method1()
+        {
+            Console.WriteLine("Method");
+            Console.ReadLine();
+        }
+
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
