2581c3a615d3d1d071d674e0686ced85a0ffc8dd
visualstudio <visualstudio@122-PC22>
2019-03-06 11:30:56 -0500

    Lab06

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -11,27 +11,38 @@ namespace DelegateAndEvents
 //Assignment 2
 //March 6-2019
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
+            Console.ReadLine();
         }
     }
     public class DelegateExercises
     {
-        //Name : Gurkirat Singh (C0731779)
-        //Name : Robindeep Singh (C0723381)
-        public delegate int MyDelegate();
-        void Method1()
+        public delegate int MyDelegate(int intValue);
+        int Method1(int intMethod1)
         {
-            System.Console.WriteLine("MyDelegate");
+            return intMethod1 * 2;
         }
+        int Method2(int intMethod1)
+        {
+            return intMethod1 * 10;
+        }
+        public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            Console.WriteLine(result);
+        }
+
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
         }
     }
 }
