b325a088f6eee0cc5ce2dcc8dc77aaf70b945306
visualstudio <visualstudio@122-PC22>
2019-03-06 11:59:11 -0500

    Lab12

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,27 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DelegateAndEvents
- //Student Name:- Gurkirat Singh Student Id:-C0731779
+//Student Name:- Gurkirat Singh Student Id:-C0731779
 //student Name:-Robindeep Singh  Student Id:- C0723381
 // CSD 3354 Section 2
 //Assignment 2
 //March 6-2019
 {
+    using System;
+    delegate void ExampleDelegate(string xyz);
     public class Program
     {
-        static void Main(string[] args)
+        public static void Method1(string xyz)
         {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
+            Console.WriteLine(xyz + " Method1");
         }
-    }
-    public delegate void MyDelegate();
-    public class DelegateExercises
-    {
-        public void Method3()
+        public static void Method2(string xyz)
         {
-            System.Console.WriteLine(MyDelegate.ToString());
+            Console.WriteLine(xyz + " Method2");
+        }
+        public static void Main()
+        {
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+            ex1Delegate = new ExampleDelegate(Method1);
+            ex2Delegate = new ExampleDelegate(Method2);
+            ex3Delegate = ex1Delegate + ex2Delegate;
+            myDelegate = ex1Delegate = ex2Delegate;
+            ex1Delegate("AAA");
+            ex2Delegate("BBB");
+            ex3Delegate("CCC");
+            myDelegate("DDD");
+            myDelegate = ex3Delegate - ex1Delegate;
+            myDelegate("EEE");
+            myDelegate = ex3Delegate - ex2Delegate;
+            myDelegate("FFF");
+            Console.ReadLine();
+            
         }
     }
 }
